30fb2bb87b9caf7b4fb9e06c87ecbede2377f3d9
Alan <allan-1963@hotmail.com>
2025-05-27 20:46:41 -0500

    created header area and mission stmt cards

---
+++ src/components/header.jsx
@@ -0,0 +1,19 @@
+
+//component import
+//import Logo from '../assets/logo.svg'
+
+//style import
+import '../styles/header.css'
+
+function Header() {
+
+  return (
+    <>
+      <header className='header'>
+        {/* <img src={Logo} alt='logo' className='logo'/> */}
+      </header>
+    </>
+  )
+}
+
+export default Header
---
+++ src/components/mission-statement.jsx
@@ -0,0 +1,27 @@
+//react imports
+
+
+//component imports
+import MissionImg from '../assets/mission-img.jpg'
+
+//style import
+import '../styles/mission-statement.css'
+
+function Mission() {
+
+  return (
+    <>
+      <article className='mission-cards'>
+        <div className='mission'>
+          <h2>Personal Injury <br/> Representation You <br/> Deserve</h2>
+          <p>Trust Cisneros Injury Lawyers to fight for you rights</p>
+        </div>
+        <div className='mission-img'>
+          <img src={MissionImg} className='mission-img' alt='Personal Injury Image'/>
+        </div>
+      </article>
+    </>
+  )
+}
+
+export default Mission
