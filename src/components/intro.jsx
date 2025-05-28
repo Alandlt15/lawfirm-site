7e56d3916eae548aa03f64e5b6a4c62ca702a6bc
Alan <allan-1963@hotmail.com>
2025-05-28 17:22:27 -0500

    added intro section

---
+++ src/components/intro.jsx
@@ -0,0 +1,25 @@
+//react imports
+
+
+//component imports
+
+
+//style import
+import '../styles/intro.css'
+
+function Intro() {
+
+  return (
+    <>
+      <article className='intro-card'>
+          <h2>Meet Joel Cisneros</h2>
+          <div className='intro'>
+            <p>Trust Cisneros Injury Law to fight for you rights</p>
+            <p>Image of Joel Here</p>
+          </div>
+      </article>
+    </>
+  )
+}
+
+export default Intro
